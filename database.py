926348aa9b200930732e9496b182bdad6c49b3a3
--list <sk5518@srmist.edu.in>
2022-12-26 14:57:52 +0530

    final edit

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
+import tkinter.messagebox
+
 import mysql.connector as connector
-from datetime import date
+from datetime import datetime
 
 def establishConnection(user,passwd):
     try:
@@ -19,27 +21,21 @@ def establishConnection(user,passwd):
 def createTable():
 
     # Creating a table
+    now = datetime.now()
     global table_name
-    table_name = "_" + date.today().strftime("%Y_%m_%d")
+    table_name = "_" + now.strftime("%m_%d_%Y_%H_%M_%S")
 
     create_table = """CREATE TABLE IF NOT EXISTS {}(
     ITEMS VARCHAR(30),
     WEIGHT FLOAT(8,2),
     MC VARCHAR(10),
     RATE FLOAT(8,2),
-    AMOUNT FLOAT(8,2));""".format(table_name)
+    AMOUNT FLOAT(8,2),
+    id int auto_increment,
+    primary key(id));""".format(table_name)
 
     cur.execute(create_table)
-
-    cur.execute("""SELECT COUNT(*)
-        FROM information_schema.tables
-        WHERE table_name = '{0}' """.format(table_name))
-
-    if cur.fetchone()[0] == 1:
-        con.commit()
-        return 1
-
-    return 0
+    con.commit()
 
 def showTablesList():
     cur.execute('show tables')
@@ -49,7 +45,83 @@ def showTablesList():
             tables.append(j)
     return tables
 
+def col_names(tname):
+    cur.execute("describe {};".format(tname))
+    col_names = [x[0] for x in cur.fetchall()]
+    return col_names
+
 def printBill(tname):
     cur.execute('select * from {};'.format(tname))
     bill=cur.fetchall();
     return bill
+
+def total_amount(tname):
+    cur.execute("SELECT SUM(Amount) FROM {};".format(tname))
+    tot = cur.fetchall()[0][0]
+    return tot
+
+def modify(tname, req):
+    count = 0
+    if req.get("action1") == "make changes":
+        col_name = col_names(tname)
+        cur.execute("select * from {};".format(tname))
+        data = cur.fetchall()
+        i=0
+        for col in col_name:
+            mitems = req.getlist(col)
+            cur.execute("select {} from {};".format(col,tname))
+            col_values = [x[0] for x in cur.fetchall()]
+            print(mitems,col_values)
+            if len(req.getlist("new")) == 0:
+                for i in range(len(mitems)):
+                    if mitems[i] != str(col_values[i]):
+                            if type(col_values[i]) == str:
+                                cur.execute("""update {}
+                                                set {}='{}'
+                                                where id={};
+                                            """.format(tname, col, mitems[i], data[i][col_name.index("id")]))
+                            else:
+                                cur.execute("""update {}
+                                                set {}={}
+                                                where id={};
+                                            """.format(tname, col, mitems[i], data[i][col_name.index("id")]))
+                            con.commit()
+
+            else:
+                mitems = req.getlist("new")
+                fin_list = [checknchange(x) for x in mitems[i:i+5]]
+                i = i+5
+                try:
+                    print(fin_list)
+                    query = "insert into {} values ('{}',{},{},{},{},0);".format(tname,*fin_list)
+                    print(query)
+                    cur.execute(query)
+                    con.commit()
+                    count += 1
+                except:
+                    pass
+
+    return count
+def remove_table(tname):
+    cur.execute("drop table {};".format(tname))
+    con.commit()
+def compare_lists(l1, l2):
+    for i in range(len(l1)):
+        if l1[i] != l2[i]:
+            return i
+    return -1
+
+def remove_row(tname,req):
+    id = list(dict(req).keys())[0]
+    cur.execute("DELETE FROM {} WHERE id = {};".format(tname, int(id)))
+    con.commit()
+
+def checknchange(n):
+    if n == "":
+        return "null"
+
+    try:
+        result = int(n)
+    except:
+        result = n
+    return result
--- server.py
+++ server.py
@@ -2,36 +2,88 @@ from flask import Flask, render_template, request, redirect, url_for
 import database
 
 app = Flask(__name__)
-
-
+nrcount = 0
 @app.route('/')
 def launch():
     return render_template('login.html')
 
-
 @app.route('/form_login', methods=['POST', 'GET'])
 def authenticate():
+    global temp_tname
     user = request.form["username"]
     passwd = request.form["password"]
     if database.establishConnection(user, passwd) == 1:
-        return render_template('home.html')
+        return redirect(url_for('dashboard'))
     else:
         return render_template('login.html', status="Invalid Credentials")
 
-@app.route('/create_bill',methods=['POST','GET'])
-def create_bill():
+@app.route('/dashboard', methods=['POST', 'GET'])
+def dashboard():
+    return render_template("home.html")
+@app.route('/modify_bill',methods=['POST','GET'])
+def modify_bill():
+    return render_template('modifybills.html', bills=database.showTablesList(), content='Bills', visibilty="invisible", size=nrcount)
+@app.route('/new_tbl')
+def new_tbl():
     database.createTable()
-    return redirect(url_for('view_tables'))
-
+    return redirect(url_for('modify_bill'))
 @app.route('/view_tables', methods=['POST', 'GET'])
 def view_tables():
-    return render_template('bills.html', bills=database.showTablesList(),content='Bills',status='bill_table_hide')
+    return render_template("bills.html", bills=database.showTablesList(),content='Bills')
 
 @app.route('/view_tables/bills', methods=['POST', 'GET'])
 def viewBills():
-    req=request.form.get("action")
-    rows = database.printBill(req)
-    return render_template('invoice.html',rows=rows)
+    temp_tname=request.form.get("action")
+    rows = database.printBill(temp_tname)
+    return render_template('invoice.html', rows=rows,bills=database.showTablesList(),content='Bills', total=database.total_amount(temp_tname))
+
+@app.route('/table_name', methods=['POST', 'GET'])
+def vBills():
+    global temp_tname
+    temp_tname = request.form.get("action")
+    rows = database.printBill(temp_tname)
+    col_names = database.col_names(temp_tname)
+    col_names.remove('id')
+    return render_template('modifybills.html', rows=rows, bills=database.showTablesList(), content='Bills', col_names = col_names, visibilty="wrapper", size=nrcount)
+@app.route('/modify_table', methods=['POST', 'GET'])
+def modify_bills():
+    global temp_tname,nrcount
+    flag = database.modify(temp_tname, request.form)
+    nrcount-=flag
+    rows = database.printBill(temp_tname)
+    col_names = database.col_names(temp_tname)
+    col_names.remove('id')
+    return render_template('modifybills.html', rows=rows, bills=database.showTablesList(), content='Bills', col_names = col_names, size=nrcount)
+@app.route('/remove',methods=["POST","GET"])
+def remove_table():
+    global temp_tname
+    database.remove_table(temp_tname)
+    return redirect(url_for("modify_bill"))
+@app.route('/plus')
+def add_row():
+    global nrcount
+    rows = database.printBill(temp_tname)
+    col_names = database.col_names(temp_tname)
+    col_names.remove('id')
+    nrcount+=1
+    return render_template('modifybills.html', extras=[i for i in range(nrcount)], rows=rows,
+                           bills=database.showTablesList(), content='Bills', col_names=col_names, size=nrcount)
+
+@app.route('/remove_row',methods=['POST','GET'])
+def rem_row():
+    global temp_tname
+    value = list(dict(request.form).values())[0]
+    if value == "x":
+        database.remove_row(temp_tname,request.form)
+        rows = database.printBill(temp_tname)
+        col_names = database.col_names(temp_tname)
+        col_names.remove('id')
+        return render_template('modifybills.html', rows=rows, bills=database.showTablesList(), content='Bills', col_names = col_names, visibilty="wrapper", size=nrcount)
+@app.route('/minus')
+def minus():
+    global nrcount
+    nrcount-=2
+    return redirect(url_for('add_row'))
 
 if __name__ == '__main__':
     app.run(debug=True)
